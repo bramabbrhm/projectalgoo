a388e7487a90334586d0460e894ef4c836c56649
bramabbrhm <abrahamvalentino.t@gmail.com>
2025-04-21 20:05:46 +0700

    add - struct, display menu, menu input data

--- project.cpp
+++ project.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
 using namespace std;
 
-void file(){
-    //ini fungsi buat add file
+struct Player {
+    char nama[50];
+    char nickname[50];
+    char role[30];
+};
+
+struct Tim {
+    char namaTim[50];
+    Player pemain[5]; 
+};
+
+void menu()
+{
+    cout << "silahkan pilih menu :" << endl;
+    cout << "1. Masukkan data " << endl;
+    cout << "2. Lihat daftar tim " << endl;
+    cout << "3. Masukkan data skor tim" << endl;
+    cout << "4. Sorting tim (berdasar skor total) " << endl;
+    cout << "5. Searching(pemain dan tim)" << endl;
+    cout << "6. Simpan hasil pertandingan " << endl;
+    cout << "7. keluar " << endl;
 }
 
-int main(int argc, char const *argv[])
+int menu1()
 {
-    cout << "hello world" << endl ;
+    Tim tim;
+    FILE *file;
+
+    file = fopen("tim.txt", "ab"); 
+    if (file == NULL) {
+        cout << "Gagal membuka file untuk menyimpan!" << endl;
+        return 1;
+    }
+
+    cin.ignore();
+    cout << "Masukkan Nama Tim: ";
+    cin.getline(tim.namaTim, 50);
+
+    for (int i = 0; i < 5; i++) {
+        cout << "\nPlayer ke-" << i + 1 << endl;
+        cout << "Nama Asli          : ";
+        cin.getline(tim.pemain[i].nama, 50);
+        cout << "Nickname in game   : ";
+        cin.getline(tim.pemain[i].nickname, 50);
+        cout << "Role               : ";
+        cin.getline(tim.pemain[i].role, 30);
+    }
+
+    fwrite(&tim, sizeof(Tim), 1, file);
+    fclose(file);
+
+    cout << "\nData tim dan 5 pemain berhasil disimpan ke tim.txt\n";
     return 0;
 }
